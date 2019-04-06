5d7b3427a3b82f901042069db3729f9f088121ec
Huubi29 <hubert.obara98@gmail.com>
2019-04-06 16:51:55 +0200

    cargo-europe-website

---
+++ Cargo Europe/js/app.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-elements');
+  const body = document.querySelector('body');
+
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    body.classList.toggle('mobile-scroll-lock')
+  })
+}
+
+navSlide();
